f9b655544d6b2b7a2de0d168cbe8f8d65f3ff853
Daniel <daniel.loaiza@rappi.com>
2020-03-12 15:42:00 -0500

    Project start, Splash screen

---
+++ app/src/main/java/com/dafelo/icecreamparlor/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.dafelo.icecreamparlor
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
